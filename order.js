7632fdb79471d230b842dec42f7c1ef1927c00ac
Vilma Kaijser <vilma.kaijser@gmail.com>
2021-03-25 17:34:18 +0100

    change quantity in local storage

--- order.js
+++ order.js
@@ -1,10 +1,8 @@
 
 $(document).ready(function(){
-
-    // skapa och anropa en load-funktion direkt
-
-    
-    const storageProducts = JSON.parse(localStorage.getItem("products"));
+   
+    let storageProducts = JSON.parse(localStorage.getItem("products"));
+    let totalPrice = 0;
     
     if(storageProducts.length == 0 || localStorage.getItem("products") === null){
         console.log(storageProducts.length);
@@ -20,7 +18,7 @@ $(document).ready(function(){
         tableStr += `<table class = "table thead-light table-striped" width=75%> <tr>
             <th>Vara</th> <th>Pris för en vara</th> <th>Totalt pris för vara</th> <th>Antal</th>
         </tr> `;
-        let totalPrice = 0;
+        
         
         for(i = 0; i < array.length; i++){
             let q = array[i].quantity;
@@ -34,18 +32,21 @@ $(document).ready(function(){
             tableStr += `<tr> <td>` + array[i].title + `</td> 
             <td class=".priceCell" > ${price} </td>
             <td class=".priceCell" > ${totalProductPrice} </td> 
-            <td> <input type="number" value=${q}  size="3" min="1" class="productQuantity"> </input> </td> </tr> `;
+            <td> <input type="number" value=${q}  size="3" min="1" class="productQuantity"> </input> </td> 
+            <td> <input type="button" value="Ta bort vara" class="removeProduct"> </input> </td></tr> `;
             totalPrice += (Number(totalProductPrice));
         };
         
         tableStr += `</table>`;
-        //console.log(tableStr);
-        //tableStr += ;
         $("#productsInCart").html(tableStr);
-        $("#totalPriceSpace").html(`<p class="text-end me-3"> Totalpris på ordern: ` + totalPrice + `</p>`);
+        $("#totalPriceLabel").html(`Totalpris på ordern: `); 
+        $("#totalPriceSpace").html(totalPrice);
         $(".productQuantity").click(changeQuantity);
+        $(".removeProduct").click(removeProduct);
   
     }
+
+
 
 
     function changeQuantity(event){
@@ -53,49 +54,53 @@ $(document).ready(function(){
         let rowIndex = tableCell.parentElement.rowIndex;
         let newQuantity = tableCell.children[0].value;
         let price = tableCell.previousElementSibling.previousElementSibling.innerText;
-        let totalPrice = $("#totalPriceSpace").innerText;
-        let oldPrice =  tableCell.previousElementSibling.value;
+        let oldPrice =  Number(tableCell.previousElementSibling.innerText);
         let newPrice = newQuantity*price;
-    
-        console.log("index " + rowIndex);
+        let priceChange = newPrice - oldPrice;
 
-        console.log("old price " + oldPrice);
+        totalPrice += priceChange; 
+       
+
+        console.log("priceCHange" + priceChange);
+        console.log("totalPrice " + totalPrice);
+        
 
         // sätt totalpris för enskild produkt och totalsumma för order
         tableCell.previousElementSibling.innerText = newPrice;
+        $("#totalPriceSpace").html(totalPrice);
 
         // spara nya kvantitet i local storage
         storageProducts[rowIndex-1].quantity = newQuantity;
-        console.log(typeof(storageProducts[0]));
-        console.log(storageProducts);
+        
         localStorage.setItem("products", JSON.stringify(storageProducts));
+        storageProducts = JSON.parse(localStorage.getItem("products"));
+
 
 
     }
      
-/*
-    $("#productsInCart").on("click", "input.productQuantity", function(){
-        let id = $(this);
-        console.log(id);
-    })
 
-    $(".productNo").change(function(){
-        let quantity = $(this).parent.value;
-        let price = $(this).parents("tr").find(".priceCell").text();
-      
-        console.log(quantity);
-        console.log(price);
-        console.dir($(this));
-        
-        let productIndex = $(this).parents("tr").find(".priceCell").text();
-        console.log($(this).parents("td").siblings(".priceCell").children());
-        console.log(productIndex.tagName);
-        console.dir(productIndex);
-    
-        // inner html när man väl kommer till rätt elem (via parent, children[typ 1])
-        //let price = this.
-    });
-   */
+    function removeProduct(event){
+        let tableCell = event.target.parentElement;
+        let rowIndex = event.target.parentElement.parentElement.rowIndex;
+        console.log(rowIndex);
+        console.log(storageProducts[rowIndex - 1]);
+
+        // nollställ värden i ordertabell
+        console.log(tableCell.previousElementSibling.children[0].value);
+        tableCell.previousElementSibling.children[0].value = 0;
+        tableCell.previousElementSibling.children[0].disabled = true;
+        totalPrice -= tableCell.previousElementSibling.previousElementSibling.innerText;
+        $("#totalPriceSpace").html(totalPrice);
+        tableCell.previousElementSibling.previousElementSibling.innerText = 0;
+
+        // ta bort vara från array
+        storageProducts.splice(rowIndex-1, 1);
+        console.log(storageProducts);
+        localStorage.setItem("products", JSON.stringify(storageProducts));
+        storageProducts = JSON.parse(localStorage.getItem("products"));
+
+    }
 
 
 
